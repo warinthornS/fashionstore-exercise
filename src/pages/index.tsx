4a7398b1fcd1241f9e647fb319de3236338fe9f4
bazsup <bas@odds.team>
2023-09-29 21:47:58 +0700

    add solution

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,8 @@
 import ProductList from "@/components/ProductList";
+import { useState } from "react";
 
 export default function Home() {
-  const productName = "";
+  const [productName, setProductName] = useState("");
   const products = [{ name: "Loose Cropped Jeans (Damaged)" }];
 
   return (
@@ -14,7 +15,14 @@ export default function Home() {
         <h1 className="text-2xl font-bold">Add a new product</h1>
         <div>
           <label htmlFor="product-name">Product name:</label>
-          <input className="bg-slate-200" id="product-name" />
+          <input
+            className="bg-slate-200"
+            id="product-name"
+            value={productName}
+            onChange={(event) => {
+              setProductName(event.target.value);
+            }}
+          />
           <p data-testid="result">{productName}</p>
         </div>
       </form>
