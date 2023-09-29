dc42d32ba6adf577cd6f06b1f0801e411ffb321a
bazsup <bas@odds.team>
2023-09-30 01:15:53 +0700

    add blink animation

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,7 +8,7 @@ const Header = () => {
         <div className="flex justify-between items-center border-b-2 border-gray-100 py-6 md:justify-start md:space-x-10">
           <div className="flex justify-start lg:w-0 flex-1">
             <Link href="/">
-              <TypingAnimation fullText="Fashion Store" />
+              <TypingAnimation fullText="Fashion Store" speed={200} />
             </Link>
           </div>
         </div>
