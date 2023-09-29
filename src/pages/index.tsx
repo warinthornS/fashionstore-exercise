838a1f47af125e4557740ef18e01c00cda657521
bazsup <bas@odds.team>
2023-09-29 22:56:50 +0700

    add solution 05

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,7 +12,7 @@ export default function Home() {
       <form
         onSubmit={(event) => {
           event.preventDefault();
-          setProducts([...products, productName]);
+          setProducts([...products, { name: productName }]);
           setProductName("");
         }}
       >
