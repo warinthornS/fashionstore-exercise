7dcad5b8480ab8acc78f0095aff5e51aa84ccad5
Warinthorn.s <boom_5912@hotmail.com>
2023-09-30 08:18:16 +0000

    Update ProductList.tsx and index.tsx

--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,13 +1,19 @@
 import Image from "next/image";
 
 // 💡 a product contains {name: string, imageUrl: string, price: number}
-const ProductList = () => {
+export interface Product {
+  name: string,
+  imageUrl: string,
+  price: number,
+}
+
+type CardDetail = {
+  item: Product
+}
+
+const Card = (detail: CardDetail) => {
   return (
     <div
-      v-else-if="productList.result.value"
-      className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
-    >
-      <div
         className="group relative"
         data-testid="product-item"
         role="listitem"
@@ -26,7 +32,7 @@ const ProductList = () => {
           <div>
             <h3 className="text-sm text-gray-700">
               <span data-testid="product-item--name" aria-hidden="true">
-                Loose Cropped Jeans (Damaged)
+                {detail.item.name}
               </span>
             </h3>
           </div>
@@ -34,7 +40,7 @@ const ProductList = () => {
             data-testid="product-item--price"
             className="text-sm font-medium text-gray-900"
           >
-            $42.57
+            $ {detail.item.price}
           </p>
         </div>
         <div className="flex space-x-2 justify-center mt-2">
@@ -46,45 +52,23 @@ const ProductList = () => {
           </button>
         </div>
       </div>
-      <div
-        className="group relative"
-        data-testid="product-item"
-        role="listitem"
-      >
-        <div className="pw-full min-h-80 bg-gray-200 aspect-w-1 aspect-h-1 rounded-md overflow-hidden group-hover:opacity-75 lg:h-80 lg:aspect-none">
-          <Image
-            data-testid="product-item--image"
-            src="https://image.uniqlo.com/UQ/ST3/AsianCommon/imagesgoods/455844/item/goods_09_455844.jpg?width=300"
-            width={300}
-            height={300}
-            alt="Smart Skort Solid"
-            className="w-full h-full object-center object-cover lg:w-full lg:h-full"
-          />
-        </div>
-        <div className="mt-4 flex justify-between">
-          <div>
-            <h3 className="text-sm text-gray-700">
-              <span data-testid="product-item--name" aria-hidden="true">
-                Smart Skort Solid
-              </span>
-            </h3>
-          </div>
-          <p
-            data-testid="product-item--price"
-            className="text-sm font-medium text-gray-900"
-          >
-            $140.37
-          </p>
-        </div>
-        <div className="flex space-x-2 justify-center mt-2">
-          <button
-            data-testid="product-item--add-to-cart"
-            className="add-to-cart-button px-6 py-1 transition ease-in duration-200 w-full rounded-full hover:bg-gray-800 hover:text-white border-2 border-gray-900 focus:outline-none"
-          >
-            Add to cart
-          </button>
-        </div>
-      </div>
+  )
+}
+
+type detail = {
+  list: Product[]
+}
+
+const ProductList = (detail: detail) => {
+  
+  return (
+    <div
+      v-else-if="productList.result.value"
+      className="mt-6 grid grid-cols-1 gap-y-10 gap-x-6 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8"
+    >
+       {detail.list.map((l) => (
+          <Card key={l.name} item={l}/>
+      ))}
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
 import ProductList from "@/components/ProductList";
+import {Product} from  "@/components/ProductList";
 
 export default function Home() {
-  return <ProductList />;
+  const p1 : Product = {
+    name: "11111",
+    imageUrl: "",
+    price: 1112
+  }
+  const p2: Product  = {
+    name: "22222",
+    imageUrl: "",
+    price: 22233
+  }
+
+  const pl = [p1, p2]
+
+  return <ProductList  list={pl}/>;
 }
