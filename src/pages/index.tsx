48b3f747315985dc525bfad9582f717b92e4bcd5
bazsup <bas@odds.team>
2023-10-07 04:35:22 +0700

    solution 15

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,18 @@
 import ProductList from "@/components/product/ProductList";
 import { useCart } from "@/contexts/cart";
 import { Product } from "@/models/product";
+import { addProductToCart } from "@/services/cart";
 import { getProducts } from "@/services/product";
+import { cartId } from "@/tools/config";
 import { useEffect, useState } from "react";
 
 export default function Home() {
   const [products, setProducts] = useState<Product[]>([]);
   const [isLoading, setIsLoading] = useState(false);
   const cart = useCart();
-  const onAddProduct = (productId: string) => {
+  const onAddProduct = async (productId: string) => {
     cart.addNumberOfItems(1);
+    await addProductToCart({ cartId: cartId, productId: productId });
     console.log(`add product:${productId} to the cart.`);
   };
 
