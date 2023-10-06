3c724709d49be0f17ac3797767e78365df6b5422
bazsup <bas@odds.team>
2023-10-07 03:28:17 +0700

    wrap with cart provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,14 @@
 import Layout from "@/components/Layout";
+import { CartProvider } from "@/contexts/cart";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <CartProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </CartProvider>
   );
 }
