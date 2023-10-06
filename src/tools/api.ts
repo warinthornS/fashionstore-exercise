837e62f57a75e3e255f70c0496c9c96b36f454d7
bazsup <bas@odds.team>
2023-10-07 03:50:46 +0700

    format

--- src/tools/api.ts
+++ src/tools/api.ts
@@ -1,6 +1,6 @@
-import axios from 'axios'
-import * as config from './config'
+import axios from "axios";
+import * as config from "./config";
 
 export const api = axios.create({
-    baseURL: config.BASE_URL
-})
+  baseURL: config.BASE_URL,
+});
